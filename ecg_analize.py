668f361125437179c65c1c8dc7e6fa4209327600
Ivan Mysin <imysin@mail.ru>
2016-10-18 15:55:43 +0300

    work, but with problem

--- ecg_analize.py
+++ ecg_analize.py
@@ -9,13 +9,8 @@ from scipy.signal import argrelmax
 import artifitial_gererator as gen
 from itertools import combinations
 from scipy.spatial.distance import mahalanobis
-# from biosppy.signals import ecg
-if __name__ == "__main__":
-    print('Simulating heart ecg')
-    length = 600        # sec
-    sampling_rate = 500 # sample in sec
-    datasetfile = "dataset.csv"
-    ecg = gen.get_ecg(length, sampling_rate)
+
+def get_health_by_ecg(ecg, sampling_rate, dataset, verbose=True):
     th = 7 * np.median( np.abs(ecg) )
     # out = ecg.ecg(signal=ecg_signal, sampling_rate=1000., show=True)
     
@@ -61,25 +56,45 @@ if __name__ == "__main__":
     #print (dcstr)
     codogram = dict()
     codogram_arr = np.array([])
+    idx = 0
+    
+    
     for codon in combinations(dcstr, 3):
         key = codon[0] + codon[1] + codon[2]
         codogram[key] = dcstr.count(key) / (dR.size - 3)
         codogram_arr = np.append(codogram_arr, codogram[key])
-        print (key)
-    
-    dataset = np.loadtxt(datasetfile, delimiter=",")
-    Vinv = np.linalg.inv(np.cov(dataset))
+        print (idx)
+        if idx >= 215:
+            break
+        idx += 1
+   
+    Vinv = np.linalg.inv(np.cov(dataset.T))
     health = mahalanobis(codogram_arr, np.mean(dataset, axis=0), Vinv)
     
+    if (verbose):
+        t = np.linspace(0, length, ecg.size)
+        
+        # Plot the sampled ecg signal
+        plt.figure()
+        plt.plot(t, ecg)
+        plt.plot([t[0], t[-1]], [th, th], "r")
+        plt.scatter(t[peaks_idx], ecg[peaks_idx], color="red")
+        plt.xlim(0, 10)
+        plt.xlabel('Time, sec')
+        plt.ylabel('Amplitude')
     
-    t = np.linspace(0, length, ecg.size)
+    return (health)
+
+
+# from biosppy.signals import ecg
+if __name__ == "__main__":
+    print('Simulating heart ecg')
+    length = 600        # sec
+    sampling_rate = 500 # sample in sec
+    datasetfile = "dataset.csv"
+    ecg = gen.get_ecg(length, sampling_rate)
+    dataset = np.loadtxt(datasetfile, delimiter=",") / 99
     
-    # Plot the sampled ecg signal
-    plt.figure()
-    plt.plot(t, ecg)
-    plt.plot([t[0], t[-1]], [th, th], "r")
-    plt.scatter(t[peaks_idx], ecg[peaks_idx], color="red")
-    plt.xlim(0, 10)
-    plt.xlabel('Time, sec')
-    plt.ylabel('Amplitude')
+    health = get_health_by_ecg(ecg, sampling_rate, dataset)
+    print (health)
     
